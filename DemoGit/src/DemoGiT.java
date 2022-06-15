e7d65dbdc4ec70cdc69661a3f440e3382311afdf
user <user@DESKTOP-ODF5BLR>
2022-06-15 11:16:14 +0530

    By Amol

--- DemoGit/src/DemoGiT.java
+++ DemoGit/src/DemoGiT.java
@@ -1,6 +1,17 @@
 
 public class DemoGiT
 {
+	
+	public void m1()
+	{
+		
+		System.out.println("Hello Amol");
+	}
+	
+	
+	
+	
+	
 public static void main(String[] args)
 {
 System.out.println("Hello");	
